02d2b43aefd139986c4c21d0e52a2c119d483bc6
SeoungYeop-Baek <tmdduq1999@naver.com>
2024-01-11 22:02:43 +0900

    3day

---
+++ 11050.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    if (n <= 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int N, K;
+    
+    cin >> N >> K;
+
+    int result = factorial(N) / (factorial(K) * factorial(N - K));
+
+    cout << result << endl;
+
+    return 0;
+}
